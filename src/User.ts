05c30f59913a24f734dce17f6a1f735335fb6272
Jot Kharoud <apple@Apples-MacBook-Pro.local>
2021-06-06 02:52:03 -0700

    first commit

---
+++ src/User.ts
@@ -0,0 +1,25 @@
+import faker from 'faker'
+
+export class User {
+    name: string;
+    location :{
+        lat: number;
+        lng : number;
+    }
+
+
+    constructor(){
+
+        this.name = faker.name.firstName()
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+
+
+    }
+
+    markerContent () : string{
+        return `Username is ${this.name}`
+    }
+}
